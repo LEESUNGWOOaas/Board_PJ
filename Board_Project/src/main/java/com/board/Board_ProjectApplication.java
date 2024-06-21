5828d9306bc605ed785723e31761ca0296a64edd
이성우 <sa775628@gmail.com>
2024-06-21 15:36:23 +0900

    first commit

---
+++ Board_Project/src/main/java/com/board/Board_ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board_ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board_ProjectApplication.class, args);
+	}
+
+}
